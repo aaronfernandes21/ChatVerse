d4ab1b27272012d839b6c96203dd6893a1e7772e
aaronfernandes21 <aaronfernandescks2015@gmail.com>
2025-06-15 23:58:30 +0530

    SignUp, ProfilePage, LoginPage and BackendCors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,46 @@
+import Navbar from "./components/Navbar";
+import {Routes,Route, Navigate} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import  { useEffect } from 'react';
+
+import {Loader} from "lucide-react";
+import {Toaster} from "react-hot-toast";
+
+
 const App = () => {
+const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+useEffect(()=>{
+  checkAuth()
+}, [checkAuth]
+);
+console.log({authUser});
+
+if(isCheckingAuth && !authUser ) return(
+  <div className="flex items-center justify-center h-screen">
+  <Loader className="size-10 animate-spin"/>
+  </div>
+)
+  //axiosInstance.
   return (
-    <div className="bg-black text-red-500 text-3xl p-6 font-bold">
-      Tailwind should style this ✅
+    <div >
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element ={authUser ?<HomePage/>: <Navigate to="/login"/>}/>
+        <Route path="/signup" element ={!authUser ?<SignUpPage/>: <Navigate to="/"/>}/>
+        
+        <Route path="/login" element ={!authUser ?<LoginPage/>: <Navigate to="/"/>}/>
+        <Route path="/settings" element ={<SettingsPage/>}/>
+        <Route path="/profile" element ={authUser ?<ProfilePage/>: <Navigate to="/login"/>}/>
+
+      </Routes>
+      <Toaster/>
     </div>
   );
 };
