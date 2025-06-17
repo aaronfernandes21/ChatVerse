2fc5334ac5d356123a42d59605e3b179b665ab0b
aaronfernandes21 <aaronfernandescks2015@gmail.com>
2025-06-18 01:10:06 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,9 +11,9 @@ app.use(express.json({ limit: "10mb" })); // or whatever size fits your needs
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 dotenv.config();
-
+import path from "path";
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -22,6 +22,16 @@ app.use(cors({
 }))
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res)=>{
+res.sendFile(path.join(__dirname, "../frontend", "dist","index.html"));
+    })
+}
 
 server.listen(PORT,()=>{
     console.log("Server is running on the port PORT: "+PORT); 
